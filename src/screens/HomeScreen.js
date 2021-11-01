df78d1d418a677980ef3f4acaf83c8a0bfb93b5c
Victor <lausellv@yahoo.com>
2021-11-01 09:29:11 +0100

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Text, StyleSheet, View, Button } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+
+// to condense the code we can refer directly to the navigation feature and remove props (=destructuring)
+const HomeScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate("Components");
+          
+        }}
+        title = "Go to my Components Demo"
+      />
+<Button
+        onPress={() => {
+          navigation.navigate("List");
+          
+        }}
+        title = "Go to List Demo"
+      />
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
