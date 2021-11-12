2ea3396202b7fb34b0bd68a8ab8826a682b19eaf
Victor <lausellv@yahoo.com>
2021-11-12 21:27:36 +0100

    imported components, added routes

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-import HomeScreen from './src/screens/HomeScreen';
-import ComponentsScreen from './src/screens/ComponentsScreen';
-import ListScreen from './src/screens/ListScreen';
-import ImageScreen from './src/screens/ImageScreen';
-import CounterScreen from './src/screens/CounterScreen';
-import ColorScreen from './src/screens/ColorScreen';
+import HomeScreen from "./src/screens/HomeScreen";
+import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListScreen from "./src/screens/ListScreen";
+import ImageScreen from "./src/screens/ImageScreen";
+import CounterScreen from "./src/screens/CounterScreen";
+import ColorScreen from "./src/screens/ColorScreen";
+import CounterScreen2 from "./src/screens/CounterScreen2";
+import ColorScreen2 from "./src/screens/ColorScreen2";
+import SquareScreen from "./src/screens/SquareScreen";
 
 const navigator = createStackNavigator(
   {
@@ -15,13 +18,16 @@ const navigator = createStackNavigator(
     List: ListScreen,
     Image: ImageScreen,
     Counter: CounterScreen,
+    Counter2: CounterScreen2,
     Color: ColorScreen,
+    Color2: ColorScreen2,
+    Square: SquareScreen
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: 'App',
-    },
+      title: "App"
+    }
   }
 );
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,30 +1,20 @@
-import React from 'react';
-import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hi there!</Text>
-      <Button
-        onPress={() => navigation.navigate('Components')}
-        title="Go to Components Demo"
-      />
-      <Button
-        title="Go to List Demo"
-        onPress={() => navigation.navigate('List')}
-      />
-      <Button
-        title="Go to Image Demo"
-        onPress={() => navigation.navigate('Image')}
-      />
-      <Button
-        title="Go to Counter Demo"
-        onPress={() => navigation.navigate('Counter')}
-      />
-      <Button
-        title="Go to Color Demo"
-        onPress={() => navigation.navigate('Color')}
-      />
+      <Button onPress={() => navigation.navigate("Components")} title="Go to Components Demo" />
+      <Button title="Go to List Demo" onPress={() => navigation.navigate("List")} />
+      <Button title="Go to Image Demo" onPress={() => navigation.navigate("Image")} />
+
+      <Button title="Go to Counter Demo" onPress={() => navigation.navigate("Counter")} />
+      <Button title="Go to Counter 2 Demo" onPress={() => navigation.navigate("Counter2")}></Button>
+      <Button title="Go to Color Demo" onPress={() => navigation.navigate("Color")} />
+      <Button title="Go to Color Screen 2" onPress={()=>navigation.navigate("Color2")}/>
+      <Button title="Go to Color Square Demo" onPress={()=>navigation.navigate("Square")}/>
+   
     </View>
   );
 };
